df2f3841478bce4653f53519a18a25903edbc21c
nizaralloul7 <nizaralloul7@outlook.fr>
2023-01-10 01:54:06 +0100

    heroku

--- src/main/java/com/example/adminservice/controllers/AdminController.java
+++ src/main/java/com/example/adminservice/controllers/AdminController.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/admin")
-@CrossOrigin(origins = "http://localhost:4200", methods = { RequestMethod.GET, RequestMethod.POST })
 public class AdminController
 {
     private final AdminService  adminService;
